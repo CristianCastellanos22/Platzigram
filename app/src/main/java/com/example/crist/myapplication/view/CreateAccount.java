6a445b2b16b96e7fe54c6aed3058aadab763ed49
Cristian Castellanos <cristiancastellanos7@gmail.com>
2018-07-24 13:02:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/crist/myapplication/view/CreateAccount.java
@@ -0,0 +1,15 @@
+package com.example.crist.myapplication.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.crist.myapplication.R;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
